8a3727fbfe20752c5df1538f56209e30b9921bda
pirvudoru <pirvu.doru@ymail.com>
2021-06-25 23:19:14 +0300

    Remove swagger config

--- TeacherWorkout.Api/Startup.cs
+++ TeacherWorkout.Api/Startup.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace TeacherWorkout.Api
 {
@@ -26,12 +22,7 @@ namespace TeacherWorkout.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TeacherWorkout.Api", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +31,6 @@ namespace TeacherWorkout.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TeacherWorkout.Api v1"));
             }
 
             app.UseHttpsRedirection();
