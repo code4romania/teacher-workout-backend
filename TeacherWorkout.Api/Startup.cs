d5fa4ce18701bda2fbc9c2d228a9b5357598dcb1
pirvudoru <pirvu.doru@ymail.com>
2021-06-26 14:34:22 +0300

    Allow any client to use the API

--- TeacherWorkout.Api/Startup.cs
+++ TeacherWorkout.Api/Startup.cs
@@ -23,6 +23,16 @@ namespace TeacherWorkout.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+            
             services.AddControllers();
             
             services.AddSingleton<TeacherWorkoutQuery>();
@@ -41,6 +51,8 @@ namespace TeacherWorkout.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
