5916e149e311fbe4a4509fdc061b5315ee3ba639
Alex Damian Negru <alex.negru@agilefreaks.com>
2021-06-26 18:22:05 +0300

    Add complete mock StepComplete

--- TeacherWorkout.Api/GraphQL/TeacherWorkoutMutation.cs
+++ TeacherWorkout.Api/GraphQL/TeacherWorkoutMutation.cs
@@ -29,20 +29,13 @@ namespace TeacherWorkout.Api.GraphQL
                 arguments: new QueryArguments(
                     new QueryArgument<NonNullGraphType<StepCompleteInput>> {Name = "input"}
                 ),
-                resolve: context => new StepCompletePayload()
+                resolve: context =>
                 {
-                    Step = new SlideStep
+                    var stepId = context.GetArgument<StepComplete>("input").StepId;
+                    return new StepCompletePayload()
                     {
-                        Id = "1",
-                        Title = "My title 1",
-                        Description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum;",
-                        Image = new Image
-                        {
-                            Description = "Cat Photo",
-                            Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
-                        },
-                        PreviousStep = null
-                    }
+                        Step = StepCompleteResolver.MockStepComplete(stepId)
+                    };
                 });
         }
     }
---
+++ TeacherWorkout.Api/Models/StepComplete.cs
@@ -0,0 +1,7 @@
+namespace TeacherWorkout.Api.Models
+{
+    public class StepComplete
+    {
+        public string StepId { get; set; }
+    }
+}
