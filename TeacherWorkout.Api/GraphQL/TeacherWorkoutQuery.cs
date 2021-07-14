c5b9fa04407000d1f00b78bd397d59ecc7b94b73
Ana Ciufu <ciufu.ana.maria@gmail.com>
2021-07-14 16:40:34 +0300

    added dummy items in query lists

--- TeacherWorkout.Api/GraphQL/TeacherWorkoutQuery.cs
+++ TeacherWorkout.Api/GraphQL/TeacherWorkoutQuery.cs
@@ -60,6 +60,76 @@ namespace TeacherWorkout.Api.GraphQL
                             Description = "More Cat Photos",
                             Url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReLDHsAIhgLgFpupyZg0CtevFcI2NY9WkoOQ&usqp=CAU"
                         }
+                    },
+                    new()
+                    {
+                        Id = "5",
+                        Title = "Doloremque laudantium",
+                        Thumbnail = new Image
+                        {
+                            Description = "Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/e/e6/10_years_old_american_staff.jpg"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "6",
+                        Title = "Quasi architecto beatae vitae",
+                        Thumbnail = new Image
+                        {
+                            Description = "More Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "7",
+                        Title = "Nemo enim ipsam voluptatem",
+                        Thumbnail = new Image
+                        {
+                            Description = "Cute Dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/AiredaleDog.jpg/386px-AiredaleDog.jpg"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "8",
+                        Title = "Sed quia consequuntur magni dolores eos",
+                        Thumbnail = new Image
+                        {
+                            Description = "Nice dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Sitting_dogs#/media/File:Cachorro_ra%C3%A7a_Lhasa_Apso_posando_para_book_canino_perfil.JPG"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "9",
+                        Title = "Ratione voluptatem sequi nesciunt",
+                        Thumbnail = new Image
+                        {
+                            Description = "Very nice dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paisible.JPG"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "10",
+                        Title = "Neque porro quisquam est",
+                        Thumbnail = new Image
+                        {
+                            Description = "Small dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Dogs_tilting_head#/media/File:Yumi_19mois2.jpg"
+                        }
+                    },
+                    new()
+                    {
+                        Id = "11",
+                        Title = "Accusamus et iusto",
+                        Thumbnail = new Image
+                        {
+                            Description = "Beautiful dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Quality_images_of_dogs#/media/File:Canis_lupus_PO.jpg"
+                        }
                     }
                 }.ToConnection());
             
@@ -167,6 +237,181 @@ namespace TeacherWorkout.Api.GraphQL
                         Duration = new Duration
                         {
                             Value = 37,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "5",
+                        Title = "Doloremque laudantium",
+                        Thumbnail = new Image
+                        {
+                            Description = "Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/e/e6/10_years_old_american_staff.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "7",
+                            Title = "Nemo enim ipsam voluptatem",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cute Dog photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/AiredaleDog.jpg/386px-AiredaleDog.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 39,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "6",
+                        Title = "Quasi architecto beatae vitae",
+                        Thumbnail = new Image
+                        {
+                            Description = "More Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "8",
+                            Title = "Sed quia consequuntur magni dolores eos",
+                            Thumbnail = new Image
+                            {
+                                Description = "Nice dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Sitting_dogs#/media/File:Cachorro_ra%C3%A7a_Lhasa_Apso_posando_para_book_canino_perfil.JPG"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 42,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "7",
+                        Title = "Nemo enim ipsam voluptatem",
+                        Thumbnail = new Image
+                        {
+                            Description = "Cute Dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/AiredaleDog.jpg/386px-AiredaleDog.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "9",
+                            Title = "Ratione voluptatem sequi nesciunt",
+                            Thumbnail = new Image
+                            {
+                                Description = "Very nice dog photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paisible.JPG"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 45,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "8",
+                        Title = "Sed quia consequuntur magni dolores eos",
+                        Thumbnail = new Image
+                        {
+                            Description = "Nice dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Sitting_dogs#/media/File:Cachorro_ra%C3%A7a_Lhasa_Apso_posando_para_book_canino_perfil.JPG"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "10",
+                            Title = "Neque porro quisquam est",
+                            Thumbnail = new Image
+                            {
+                                Description = "Small dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Dogs_tilting_head#/media/File:Yumi_19mois2.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 44,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "9",
+                        Title = "Ratione voluptatem sequi nesciunt",
+                        Thumbnail = new Image
+                        {
+                            Description = "Very nice dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paisible.JPG"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "11",
+                            Title = "Accusamus et iusto",
+                            Thumbnail = new Image
+                            {
+                                Description = "Beautiful dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Quality_images_of_dogs#/media/File:Canis_lupus_PO.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 43,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "10",
+                        Title = "Neque porro quisquam est",
+                        Thumbnail = new Image
+                        {
+                            Description = "Small dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Dogs_tilting_head#/media/File:Yumi_19mois2.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "5",
+                            Title = "Doloremque laudantium",
+                            Thumbnail = new Image
+                            {
+                                Description = "Dog Photos",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/e/e6/10_years_old_american_staff.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 46,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "11",
+                        Title = "Accusamus et iusto",
+                        Thumbnail = new Image
+                        {
+                            Description = "Beautiful dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Quality_images_of_dogs#/media/File:Canis_lupus_PO.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "6",
+                            Title = "Quasi architecto beatae vitae",
+                            Thumbnail = new Image
+                            {
+                                Description = "More Dog Photos",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 49,
                             Unit = DurationUnit.Minutes
                         }
                     }
