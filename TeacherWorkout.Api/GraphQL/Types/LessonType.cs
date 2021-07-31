2464f7767475432f623828355fd63bf94834e8f9
Radu B <radu.buciuceanu@gmail.com>
2021-07-31 15:55:57 +0300

    [feature/40] Change lesson graph type

--- TeacherWorkout.Api/GraphQL/Types/LessonType.cs
+++ TeacherWorkout.Api/GraphQL/Types/LessonType.cs
@@ -8,9 +8,10 @@ namespace TeacherWorkout.Api.GraphQL.Types
         public LessonType()
         {
             Name = "Lesson";
-            
+
             Field(x => x.Id, type: typeof(IdGraphType));
             Field(x => x.Title).Description("The title of the Lesson");
+            Field(x => x.Description).Description("The description of the Lesson");
             Field(x => x.Thumbnail).Description("The thumbnail of the Lesson");
             Field(x => x.Theme).Description("The Theme of the Lesson");
             Field(x => x.Duration).Description("The duration of the Lesson");
