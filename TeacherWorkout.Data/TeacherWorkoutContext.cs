d57454ff60fec04d56b0def73c6f3ace10c7328d
pirvudoru <pirvu.doru@ymail.com>
2021-08-26 12:49:02 +0300

    Configure how duration is converted from the database

--- TeacherWorkout.Data/TeacherWorkoutContext.cs
+++ TeacherWorkout.Data/TeacherWorkoutContext.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeacherWorkout.Data.Entities;
+using TeacherWorkout.Domain.Models;
 
 namespace TeacherWorkout.Data
 {
     public class TeacherWorkoutContext : DbContext
     {
-        public DbSet<Lesson> Lessons { get; set; }
+        public DbSet<ELesson> Lessons { get; set; }
+        public DbSet<ETheme> Themes { get; set; }
 
         public TeacherWorkoutContext(DbContextOptions<TeacherWorkoutContext> options) : base(options)
         {
+        }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            
+            var converter = new ValueConverter<Duration, int>(
+                from => from.TotalSeconds,
+                to => Duration.FromSeconds(to)
+            );
+
+            modelBuilder.Entity<Lesson>()
+                .Property(l => l.Duration)
+                .HasConversion(converter);
         }
     }
 }
