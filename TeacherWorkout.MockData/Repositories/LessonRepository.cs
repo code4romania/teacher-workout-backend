8792df90efc1b669360f8e0bbd438128234850ee
pirvudoru <pirvu.doru@ymail.com>
2021-10-16 14:23:00 +0300

    Revert "Drop mock implementation of repository"
    This reverts commit 774d67b931d66768990aeb2d5e71f00cb2b1244b.

---
+++ TeacherWorkout.MockData/Repositories/LessonRepository.cs
@@ -0,0 +1,297 @@
+﻿using System.Collections.Generic;
+using TeacherWorkout.Domain.Common;
+using TeacherWorkout.Domain.Lessons;
+using TeacherWorkout.Domain.Models;
+
+namespace TeacherWorkout.MockData.Repositories
+{
+    public class LessonRepository : ILessonRepository
+    {
+        public PaginatedResult<Lesson> PaginatedList(LessonFilter filter)
+        {
+            return new()
+            {
+                TotalCount = 11,
+                Items = new List<Lesson>
+                {
+                    new()
+                    {
+                        Id = "1",
+                        Title = "Lorem Ipsum",
+                        Thumbnail = new Image
+                        {
+                            Description = "Cat Photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
+
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "1",
+                            Title = "Lorem Ipsum",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cat Photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 45,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "2",
+                        Title = "Dolor sit amet",
+                        Thumbnail = new Image
+                        {
+                            Description = "Another Cat Photo",
+                            Url = "https://static.toiimg.com/thumb/msid-67586673,width-800,height-600,resizemode-75,imgsize-3918697,pt-32,y_pad-40/67586673.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "1",
+                            Title = "Lorem Ipsum",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cat Photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 32,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "3",
+                        Title = "Consectetur adipiscing elit",
+                        Thumbnail = new Image
+                        {
+                            Description = "YACP",
+                            Url = "http://cdn.shopify.com/s/files/1/1149/5008/articles/why-cat-looking-at-wall-or-nothing.jpg?v=1551321728"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "1",
+                            Title = "Lorem Ipsum",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cat Photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 55,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "4",
+                        Title = "Fusce tempor",
+                        Thumbnail = new Image
+                        {
+                            Description = "More Cat Photos",
+                            Url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReLDHsAIhgLgFpupyZg0CtevFcI2NY9WkoOQ&usqp=CAU"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "1",
+                            Title = "Lorem Ipsum",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cat Photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/640px-Felis_catus-cat_on_snow.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 37,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "5",
+                        Title = "Doloremque laudantium",
+                        Thumbnail = new Image
+                        {
+                            Description = "Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/e/e6/10_years_old_american_staff.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "7",
+                            Title = "Nemo enim ipsam voluptatem",
+                            Thumbnail = new Image
+                            {
+                                Description = "Cute Dog photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/AiredaleDog.jpg/386px-AiredaleDog.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 39,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "6",
+                        Title = "Quasi architecto beatae vitae",
+                        Thumbnail = new Image
+                        {
+                            Description = "More Dog Photos",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "8",
+                            Title = "Sed quia consequuntur magni dolores eos",
+                            Thumbnail = new Image
+                            {
+                                Description = "Nice dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Sitting_dogs#/media/File:Cachorro_ra%C3%A7a_Lhasa_Apso_posando_para_book_canino_perfil.JPG"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 42,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "7",
+                        Title = "Nemo enim ipsam voluptatem",
+                        Thumbnail = new Image
+                        {
+                            Description = "Cute Dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/43/AiredaleDog.jpg/386px-AiredaleDog.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "9",
+                            Title = "Ratione voluptatem sequi nesciunt",
+                            Thumbnail = new Image
+                            {
+                                Description = "Very nice dog photo",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paisible.JPG"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 45,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "8",
+                        Title = "Sed quia consequuntur magni dolores eos",
+                        Thumbnail = new Image
+                        {
+                            Description = "Nice dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Sitting_dogs#/media/File:Cachorro_ra%C3%A7a_Lhasa_Apso_posando_para_book_canino_perfil.JPG"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "10",
+                            Title = "Neque porro quisquam est",
+                            Thumbnail = new Image
+                            {
+                                Description = "Small dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Dogs_tilting_head#/media/File:Yumi_19mois2.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 44,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "9",
+                        Title = "Ratione voluptatem sequi nesciunt",
+                        Thumbnail = new Image
+                        {
+                            Description = "Very nice dog photo",
+                            Url = "https://upload.wikimedia.org/wikipedia/commons/9/9a/Paisible.JPG"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "11",
+                            Title = "Accusamus et iusto",
+                            Thumbnail = new Image
+                            {
+                                Description = "Beautiful dog photo",
+                                Url = "https://commons.wikimedia.org/wiki/Category:Quality_images_of_dogs#/media/File:Canis_lupus_PO.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 43,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "10",
+                        Title = "Neque porro quisquam est",
+                        Thumbnail = new Image
+                        {
+                            Description = "Small dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Dogs_tilting_head#/media/File:Yumi_19mois2.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "5",
+                            Title = "Doloremque laudantium",
+                            Thumbnail = new Image
+                            {
+                                Description = "Dog Photos",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/e/e6/10_years_old_american_staff.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 46,
+                            Unit = DurationUnit.Minutes
+                        }
+                    },
+                    new()
+                    {
+                        Id = "11",
+                        Title = "Accusamus et iusto",
+                        Thumbnail = new Image
+                        {
+                            Description = "Beautiful dog photo",
+                            Url = "https://commons.wikimedia.org/wiki/Category:Quality_images_of_dogs#/media/File:Canis_lupus_PO.jpg"
+                        },
+                        Theme = new Theme
+                        {
+                            Id = "6",
+                            Title = "Quasi architecto beatae vitae",
+                            Thumbnail = new Image
+                            {
+                                Description = "More Dog Photos",
+                                Url = "https://upload.wikimedia.org/wikipedia/commons/f/f6/11.10.2015_Samoyed_%28cropped%29.jpg"
+                            }
+                        },
+                        Duration = new Duration
+                        {
+                            Value = 49,
+                            Unit = DurationUnit.Minutes
+                        }
+                    }
+                }
+            };
+        }
+    }
+}
