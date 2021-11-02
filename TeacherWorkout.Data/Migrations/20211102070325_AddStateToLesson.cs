314ceb783275a7ad0c7d7c4a2438b963aa664d40
pirvudoru <pirvu.doru@ymail.com>
2021-11-02 09:04:05 +0200

    Add the state column to the lessons table

---
+++ TeacherWorkout.Data/Migrations/20211102070325_AddStateToLesson.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeacherWorkout.Data.Migrations
+{
+    public partial class AddStateToLesson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "State",
+                table: "Lessons",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "State",
+                table: "Lessons");
+        }
+    }
+}
