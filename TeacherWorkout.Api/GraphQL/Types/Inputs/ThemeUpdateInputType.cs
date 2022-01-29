50b1b74ff1271c44888f1fdd1bee322b7ba1a88c
Iulia Dormenco <31412211+decembrya@users.noreply.github.com>
2022-01-29 10:10:35 +0200

    [58] Implement theme update (#72)

---
+++ TeacherWorkout.Api/GraphQL/Types/Inputs/ThemeUpdateInputType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using TeacherWorkout.Domain.Models.Inputs;
+
+namespace TeacherWorkout.Api.GraphQL.Types.Inputs
+{
+    public class ThemeUpdateInputType : InputObjectGraphType<ThemeUpdateInput>
+    {
+        public ThemeUpdateInputType()
+        {
+            Name = "ThemeUpdateInput";
+
+            Field(x => x.Id, type: typeof(IdGraphType));
+            Field(x => x.ThumbnailId, type: typeof(IdGraphType));
+            Field(x => x.Title);
+        }
+    }
+}
--- TeacherWorkout.Data/Repositories/ThemeRepository.cs
+++ TeacherWorkout.Data/Repositories/ThemeRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using TeacherWorkout.Domain.Common;
 using TeacherWorkout.Domain.Models;
+using TeacherWorkout.Domain.Models.Inputs;
 using TeacherWorkout.Domain.Themes;
 
 namespace TeacherWorkout.Data.Repositories
@@ -31,6 +32,23 @@ namespace TeacherWorkout.Data.Repositories
         {
             _context.Themes.Add(theme);
             _context.SaveChanges();
+        }
+
+        public Theme Find(string id)
+        {
+            return _context.Themes.Find(id);
+        }
+        
+        public Theme Update(ThemeUpdateInput input)
+        {
+            var theme = _context.Themes.Find(input.Id);
+
+            var thumbnailId = input.ThumbnailId ?? theme.ThumbnailId;
+            theme.Title = input.Title;
+            theme.Thumbnail = _context.Images.Find(thumbnailId);
+
+            _context.SaveChanges();
+            return theme;
         }
     }
 }
